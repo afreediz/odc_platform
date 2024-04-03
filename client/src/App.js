db3f122bc169bb6385b37be91083c1cec375846a
afreediz <afreedisulfiker@gmail.com>
2024-04-03 19:56:10 +0530

    private routes file added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import { useContext } from "react";
 import { usercontext } from "./context/userContext";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import Home from "./pages/user/Home";
 import Login from "./pages/Login";
+import UserPrivate from "./context/UserPrivate";
+import HotelPrivate from "./context/HotelPrivate";
+import AdminPrivate from "./context/AdminPrivate";
 
 function App() {
   const { user, setUser } = useContext(usercontext)
@@ -13,9 +16,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route element={<UserPrivate />}>
+            <Route path="/user" element />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
