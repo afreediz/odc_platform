91a85e5759aaecf5b0e0d2255af54fd2b4809dce
afreediz <afreedisulfiker@gmail.com>
2024-04-03 21:36:59 +0530

    converted to api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var userRouter = require('./routes/userRoute');
 var hotelRoute=require('./routes/hotelRoute')
@@ -10,11 +11,9 @@ var jobRoute=require('./routes/jobRoute')
 
 var app = express();
 
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,12 +30,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  err.message += " " + req.url
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).json({error:err.message})
 });
 
 module.exports = app;
