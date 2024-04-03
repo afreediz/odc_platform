bd4d503fc19467644e9105c30f86c1a35ecee4f2
jayaramskumar <kumarjayaram545@gmail.com>
2024-04-03 20:15:52 +0530

    making profile page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Profile from "./pages/user/Profile";
 
 function App() {
   const { user, setUser } = useContext(usercontext)
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/user/:id" element={<Profile/>}/>
         </Routes>
       </BrowserRouter>
     </div>
