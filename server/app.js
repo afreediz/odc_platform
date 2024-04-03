6c332eaec802ec708a43b84d0f79bda03517b628
Sharath <sharathj116@gmail.com>
2024-04-03 19:25:10 +0530

    user controller completed

--- server/app.js
+++ server/app.js
@@ -10,9 +10,8 @@ var jobRoute=require('./routes/jobRoute')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
