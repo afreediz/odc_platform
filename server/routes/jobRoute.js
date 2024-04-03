664509613892813a1911c276298a2eb18f43bf29
Sharath <sharathj116@gmail.com>
2024-04-04 03:47:56 +0530

    conflic rslving in job

--- server/routes/jobRoute.js
+++ server/routes/jobRoute.js
@@ -1,16 +1,27 @@
-
+const express = require("express")
+const router = express.Router()
+const {
+    postJob,
+    getSpecificJob,
+    updateSpecificJob,
+    deleteSpecificJob,
+    bookJob,
+    unbookJob,
+    getjob,
+} = require("../controllers/jobController")
+// get all jobs
+router.get("/", getjob)
 // create job
 router.post("/", postJob)
 // get specific
 router.get("/:id", getSpecificJob)
 // upate specific job
 router.put("/:id", updateSpecificJob),
-// delete specific job
-router.delete("/:id",deleteSpecificJob)
+    // delete specific job
+    router.delete("/:id", deleteSpecificJob)
 // // book job
 // router.post('/book/:id',bookJob)
 // // unbook job
 // router.post('/cancelbook/:id',unbookJob)
-
 
 module.exports = router
