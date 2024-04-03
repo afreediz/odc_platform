e22bef888b22eca8cbc8703ed0a98215fdc850be
afreediz <afreedisulfiker@gmail.com>
2024-04-04 04:22:43 +0530

    profile data displaying done

--- client/src/pages/user/Profile.jsx
+++ client/src/pages/user/Profile.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { Table, Card } from 'flowbite-react'
 
 const Profile = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState()
   const [error, setError] = useState("")
   useEffect(() => {
     async function loadData() {
@@ -15,7 +15,7 @@ const Profile = () => {
           }
         });
         console.log(res);
-        setData(res.data.user)
+        setData(res.data)
       } catch ({ response }) {
         setError(response.data.message)
       }
@@ -28,13 +28,13 @@ const Profile = () => {
       <div className='bg-blue-400 h-screen pt-2'>
         <h1 className='md:text-4xl text-2xl font-semibold  text-white mb-3 md:w-[60%] sm:w-[80%] w-[90%] mx-auto'>Profile</h1>
         <div className='bg-white md:w-[60%] sm:w-[80%] w-[90%] mx-auto md:p-5 p-4 rounded-md mb-6'>
-          <h1 className='md:text-4xl text-2xl font-semibold mb-2' >Jayaram s kumar</h1>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>Email - <span className='font-semibold'>jayaram@gmail.com</span></p>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>phone - <span className='font-semibold'>7907144673</span></p>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>DOB - <span className='font-semibold'>13/06/2003</span></p>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>State - <span className='font-semibold'>Kerala</span></p>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>District - <span className='font-semibold'>Ernakulam</span></p>
-          <p className='text-[15px] md:text-[18px] text-gray-500'>Address - <span className='font-semibold'>House no:001 vytilla , ernakulam</span></p>
+          <h1 className='md:text-4xl text-2xl font-semibold mb-2' >{data && data.name}</h1>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>Email - {data && data.email}<span className='font-semibold'></span></p>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>phone - {data && data.phone}<span className='font-semibold'>7907144673</span></p>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>DOB - {data && data.dob}<span className='font-semibold'>13/06/2003</span></p>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>State -{data && data.state} <span className='font-semibold'>Kerala</span></p>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>District -{data && data.district} <span className='font-semibold'>Ernakulam</span></p>
+          <p className='text-[15px] md:text-[18px] text-gray-500'>Address - {data && data.address}<span className='font-semibold'>House no:001 vytilla , ernakulam</span></p>
         </div>
 
         <h1 className='md:text-3xl text-xl font-semibold  text-green-600 mb-3 md:w-[60%] sm:w-[80%] w-[90%] mx-auto'>Completed Jobs</h1>
