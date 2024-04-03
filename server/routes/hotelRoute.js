911d00d22fd842b320b2d6aa214129d56d374e66
afreediz <afreedisulfiker@gmail.com>
2024-04-03 16:46:57 +0530

    routes prototype added

--- server/routes/hotelRoute.js
+++ server/routes/hotelRoute.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const hotelcontroller=require("../controllers/hotelController")
 const router= express.Router()
-router.post("/addingHotel",hotelcontroller)
+
+// hotel reg
+router.post("/register",hotelcontroller)
+router.post('login',)
+// jobs of hotel
+router.get('/myjobs',)
+
+module.exports = router
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const usercontroller = require("../controller/usercontroller")
 const router = express.Router()
+
 router.post("/register", usercontroller.addUser)
-router.get("/login", usercontroller.userLogin)
+router.post("/login", usercontroller.userLogin)
+router.get("/profile", usercontroller.userLogin)
+router.delete("/profile",)
+router.update("/profile", usercontroller.userLogin)
+
+module.exports = router
