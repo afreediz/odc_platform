4e6d99328171f485d728a02d741209de57e24ade
afreediz <afreedisulfiker@gmail.com>
2024-04-03 15:38:08 +0530

    user login functionality done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useContext } from 'react'
+import { usercontext } from '../context/userContext'
+import { API_URL } from '../constants';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const {setUser} = useContext(usercontext);
+  const [data, setData] = useState({
+    email:"",
+    password:""
+  })
+  const [error, setError] = useState("")
+  // 2 input boxes with names : email and password targetting onChange to onchange 
+  const onchange = (e)=> {
+    const {name, value} = e.target;
+    setData((old_data)=>{
+      return {
+        [name]:value
+      }
+    })
+  }
+  const on_submit = async()=> {
+    try{
+      const response = await axios.post(API_URL+'auth/login?type=user',{
+        ...data
+      })
+      setUser(response.data.user)
+      useNavigate('/')
+    }catch({response}){
+      setError(response.data.message)
+    }
+  }
   return (
     <div>Login</div>
   )
