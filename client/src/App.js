7dcf05c591470e305984ab15ca797e49f0e6a4c8
jayaramskumar <kumarjayaram545@gmail.com>
2024-04-03 14:58:05 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import { usercontext } from "./context/userContext";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  const {user, setUser} = useContext(usercontext)
+  const { user, setUser } = useContext(usercontext)
   setUser("hi")
   console.log(user);
   return (
-    <div className="App">
-      hello
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
