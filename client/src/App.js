a03df1611dc3887f1bf851617e2f10d3c9a85b47
afreediz <afreedisulfiker@gmail.com>
2024-04-03 13:21:29 +0530

    user and job context added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+import { useContext } from "react";
+import { usercontext } from "./context/userContext";
 
 function App() {
+  const {user, setUser} = useContext(usercontext)
+  setUser("hi")
+  console.log(user);
   return (
     <div className="App">
       hello
